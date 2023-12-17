bc2bbf3fca7cdd40fdfe4ccd7b9e03fd5253ad4e
Angel Zaprianov <fire1@abv.bg>
2023-12-17 14:56:02 +0200

    Init commit

---
+++ config.h
@@ -0,0 +1,48 @@
+// Copyright 2023 Angel Zaprianov (@fire1)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+#define SPLIT_HAND_PIN F4
+#define OLED_DISPLAY_128X32
+#define SPLIT_OLED_ENABLE
+#define RGBLIGHT_LAYERS
+#define RGB_MATRIX_LED_COUNT 70
+#define DRIVER_LED_TOTAL 70
+
+//
+// RGB Matrix setup
+#define RGB_MATRIX_KEYPRESSES                                   // reacts to keypresses
+#define RGB_MATRIX_KEYRELEASES                                  // reacts to keyreleases (instead of keypresses)
+#define RGB_DISABLE_AFTER_TIMEOUT 0                             // number of ticks to wait until disabling effects
+#define RGB_DISABLE_WHEN_USB_SUSPENDED false                    // turn off effects when suspended
+#define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+#define RGB_MATRIX_LED_FLUSH_LIMIT 16                           // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 200                       // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
+#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CYCLE_LEFT_RIGHT     // Sets the default mode, if none has bggggggeen set
+
+#ifdef OLED_ENABLE
+#    define OLED_FONT_H "lib/glcdfont.c"
+#endif
+
+#define TAPPING_TERM 200
+#define TAPPING_TERM_PER_KEY
+
+// Try to get the rgb lights to turn off when the computer is sleeping. Doesn't seem to work reliably.
+#define RGBLIGHT_SLEEP
+// Enable lighting layers
+#define RGBLIGHT_LAYERS
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+#define RGBLIGHT
+
+#define RGB_MATRIX_CYCLE_PINWHEEL
+ #define RGBLIGHT_EFFECT_KNIGHT
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+// #define NO_DEBUG
+
+/* disable print */
+// #define NO_PRINT
---
+++ keymaps/default/keymap.c
@@ -0,0 +1,259 @@
+#include QMK_KEYBOARD_H
+
+
+// Keyboard layers
+enum layer_number {
+  _QWERTY = 0,
+  _LOWER,
+  _RAISE,
+  _ADJUST
+};
+
+enum custom_keycodes {
+    CHANGE_LAN=SAFE_RANGE
+};
+// TapDance
+enum {
+    TD_SFT_CPS=0,// Right shift & caps lock
+    TD_SFT_LAN,
+    TD_TAB_TCL
+};
+
+
+
+
+
+// Keymaps
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* QWERTY
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |BackSP|
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |------+------+------+------+------+------|   F5  |    |  F6   |------+------+------+------+------+------|
+ * |LCTRL |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  | RAISE|BackSP| Menu |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+//   TD(TD_ESC_GRV) HD_SFT_LAN
+ [_QWERTY] = LAYOUT(
+        KC_ESC,    KC_1,    KC_2,   KC_3,    KC_4,     KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,      KC_BSPC,
+        KC_TAB,    KC_Q,    KC_W,   KC_E,    KC_R,     KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,      KC_MINS,
+TD(TD_SFT_LAN),    KC_A,    KC_S,   KC_D,    KC_F,     KC_G,                          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,   MT(MOD_RSFT,  KC_QUOT),
+        KC_LCTL,  KC_Z,    KC_X,   KC_C,    KC_V,     KC_B,   KC_F5,           KC_F6, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   TD(TD_SFT_CPS),
+                                KC_LGUI, KC_LGUI, KC_LALT,MO(_LOWER), KC_SPC,   KC_ENT, MO(_RAISE),  KC_BSPC, KC_LGUI,   MT(MOD_RCTL, KC_APP)
+
+),
+/* LOWER
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  |  INS |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |   q  |   w  |  ^^  |   r  |   t  |                    |      |      |   *  |   +  |  |   |   =  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |   a  |  <-  |  \/  |  ->  |   g  |-------.    ,-------| Left | Down |  Up  |Right | End  |      |
+ * |------+------+------+------+------+------|   F11 |    |  F12  |------+------+------+------+------+------|
+ * |      |   z  |   x   |  MR  |  ML  |  b   |      |-------|    |-------|      | Home |   <  |  >   |  \   |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+[_LOWER] = LAYOUT(
+    _______,  KC_F1,    KC_F2,   KC_F3,    KC_F4,  KC_F5,                            KC_F6,   KC_F7,     KC_F8,   KC_F9,   KC_F10,  KC_INS,
+    _______,  _______,  _______, KC_MS_U, _______, _______,                          KC_CIRC, KC_AMPR,   KC_PAST, KC_PLUS, KC_PIPE,  KC_EQL,
+    _______,  KC_LCTL,  KC_MS_L, KC_MS_D, KC_MS_R, _______,                          KC_LEFT, KC_DOWN,   KC_UP,   KC_RGHT, KC_END, _______,
+    _______,  _______,  _______, KC_BTN2, KC_BTN1, _______, _______,       _______,  _______,  KC_HOME,  KC_LABK, KC_RABK, KC_BSLS, _______,
+                                _______,_______, _______,  _______, _______,        KC_BSPC,  _______, KC_DEL, _______,_______
+),
+
+
+
+/* RAISE
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |  DEL |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |   `  |  @   |   #  |  $   |   %  |                    |   ^  |   &  |   *  |   !  |  ?   |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |   (  |  )   |  {   |  }   |   ~  |-------.    ,-------|   5  |   6  |   7  |   8  |   9  |      |
+ * |------+------+------+------+------+------|  PgUp |    |  PgDw |------+------+------+------+------+------|
+ * |      |   !  |  _   |   [  |  ]   |      |-------|    |-------|   0  |   1  |   2  |   3  |   4  |      |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+//  KC_DQT
+[_RAISE] = LAYOUT(
+  KC_ESC,    KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,                         KC_F6,    KC_F7,     KC_F8,     KC_F9,    KC_F10,   KC_DEL,
+TD(TD_TAB_TCL),KC_GRV,KC_AT, KC_HASH,    KC_DLR,  KC_PERC,                       KC_CIRC,  KC_AMPR,   KC_ASTR,   KC_EXLM,  KC_QUES,  _______,
+  _______,   KC_LPRN,  KC_RPRN, KC_LCBR, KC_RCBR, KC_TILD,                       KC_5,     KC_6,      KC_7,      KC_8,     KC_9,     _______,
+  _______,   KC_EXLM , KC_UNDS, KC_LBRC, KC_RBRC, _______, KC_PGUP,   KC_PGDN,   KC_0,     KC_1,      KC_2,      KC_3,     KC_4,     _______,
+                    _______, _______,  _______,  _______,  KC_DEL,    _______,  _______,  _______, _______, _______
+),
+/* ADJUST
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |sleep |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------.    ,-------|      |      |RGB ON| HUE+ | SAT+ | VAL+ |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------|    |-------|      |      | MODE | HUE- | SAT- | VAL- |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE | Del  | RGUI |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+  [_ADJUST] = LAYOUT(
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SLEP,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, RGB_VAD, RGB_VAI, XXXXXXX, RGB_SPI, RGB_SPD,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       RGB_MOD, RGB_HUI, RGB_SAI, XXXXXXX, RGB_HUD, RGB_SAD,
+  XXXXXXX, XXXXXXX, XXXXXXX, RGB_M_SW,RGB_M_R, RGB_M_B, XXXXXXX,     XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX,
+                    _______, _______, _______, _______, _______,     _______,  _______, _______, _______, _______
+  ),
+
+
+};
+
+#ifdef RGBLIGHT_ENABLE
+
+void default_rgb_layer(void){
+
+
+}
+
+#endif
+
+//
+// Suspend RGB feature
+void suspend_power_down_kb(void)
+{
+    rgb_matrix_set_suspend_state(true);
+}
+
+void suspend_wakeup_init_kb(void)
+{
+    rgb_matrix_set_suspend_state(false);
+}
+
+
+//
+// Handle layer
+layer_state_t layer_state_set_user(layer_state_t state) {
+#ifdef RGBLIGHT_ENABLE
+    //
+    // Change RGB colors from layer
+    switch (get_highest_layer(state)) {
+        case 0:
+            default_rgb_layer();
+            break;
+        case _LOWER:
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+            rgblight_sethsv_noeeprom(HSV_TEAL);
+            break;
+        case _RAISE:
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+            rgblight_sethsv_noeeprom(HSV_GREEN);
+            break;
+        case _ADJUST:
+             rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+            rgblight_sethsv_noeeprom(RGB_WHITE);
+        break;
+
+        }
+
+#endif
+
+#ifdef OLED_ENABLE
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+#endif
+
+}
+
+//SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
+#ifdef OLED_ENABLE
+
+
+// When you add source files to SRC in rules.mk, you can use functions.
+const char *read_layer_state(void);
+const char *read_logo(void);
+//void set_keylog(uint16_t keycode, keyrecord_t *record);
+const char *read_keylog(void);
+const char *read_keylogs(void);
+
+void set_oled_keys(uint16_t keycode, keyrecord_t *record);
+
+
+#endif // OLED_ENABLE
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+
+  }
+
+#ifdef OLED_ENABLE
+// todo later
+ // set_oled_keys(keycode, record);
+#endif
+
+  return true; // We didn't handle other keypresses
+}
+
+//
+// Tap dance setup
+void dance_switch_lan_start (tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+      register_code(KC_LSFT);
+  } else {
+      register_code(KC_LGUI);
+      register_code(KC_SPC);
+  }
+}
+
+void dance_switch_lan_finish (tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+   unregister_code(KC_LSFT);
+  } else {
+    unregister_code(KC_LGUI);
+    unregister_code(KC_SPC);
+  }
+}
+
+
+void dance_switch_tab_start (tap_dance_state_t *state, void *user_data) {
+
+      register_code(KC_LCTL);
+      register_code(KC_TAB);
+
+}
+
+void dance_switch_tab_finish (tap_dance_state_t *state, void *user_data) {
+
+    unregister_code(KC_LCTL);
+    unregister_code(KC_TAB);
+
+}
+
+// Tab dance
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_SFT_CPS]  = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS),
+    [TD_SFT_LAN]  = ACTION_TAP_DANCE_FN_ADVANCED (NULL,dance_switch_lan_start, dance_switch_lan_finish),
+    [TD_TAB_TCL]  = ACTION_TAP_DANCE_FN_ADVANCED (NULL,dance_switch_tab_start, dance_switch_tab_finish),
+};
+
+
+
+// RBG Render
+#ifdef RGBLIGHT_ENABLE
+void keyboard_post_init_user(void) {
+  rgblight_enable_noeeprom(); // Enables RGB, without saving settings
+  default_rgb_layer();
+  // rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+}
+
+#endif
