e307d3883991bfa6335ad5c8328a0f69bbdc4f2d
Angel Zaprianov <fire1@abv.bg>
2024-01-02 17:34:55 +0200

    Adding keymap animation of the OLED

--- lib/main.c
+++ lib/main.c
@@ -16,17 +16,18 @@
 // static const char codeToMap[30]  = {' ', ' ', ' ', ' ', 0xAA, 0xAC,0xAC};
 static const char codeToChar[60] = {' ', ' ', ' ', ' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', (char)28, 0x7F, (char)27, (char)29, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ';', '\'', ' ', ',', '.', '/', ' ', ' ', ' '};
 static char       keylog[5]      = {' ', ' ', ' ', ' '};
-// static char       keymap[5]      = {' ', ' ', ' ', ' '};
 
 //
 // Logo char definitions
-static const char PROGMEM qmk_logo[] = {0x80, 0x81, 0x82, 0x83, 0xFF, 0xA0, 0xA1, 0xA2, 0xA3, 0xFF, 0xC0, 0xC1, 0xC2, 0xC3, 0xFF};
+static const char PROGMEM qmk_logo[] = {0x80, 0x81, 0x82, 0x83, 0xFF, 0xA0, 0xA1, 0xA2, 0xA3, 0xFF, 0xC0, 0xC1, 0xC2, 0xC3};
 
 static const char PROGMEM win_logo[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xbc, 0xbc, 0xbe, 0xbe, 0x00, 0xbe, 0xbe, 0xbf, 0xbf, 0xbf, 0xbf, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x07, 0x07, 0x0f, 0x0f, 0x00, 0x0f, 0x0f, 0x1f, 0x1f, 0x1f, 0x1f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 static const char PROGMEM mac_logo[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xc0, 0xf0, 0xf8, 0xf8, 0xf8, 0xf0, 0xf6, 0xfb, 0xfb, 0x38, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x07, 0x0f, 0x1f, 0x1f, 0x0f, 0x0f, 0x1f, 0x1f, 0x0f, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 static const char PROGMEM kbr_full[] = {0x86, 0x87, 0x88, 0x89, 0xFF, 0xA6, 0xA7, 0xA8, 0xA9};
+
+static char keymap[9] = {};
 
 static uint16_t startupTimer = 0;
 static bool     isInitRun    = false;
@@ -133,11 +134,12 @@ void handleLayers(led_t ledUsbState) {
 
 void drawKeyboard(void) {
     oled_set_cursor(KBR_POSITION);
-    oled_write_P(kbr_full, false);
+    oled_write(keymap, false);
 
     oled_set_cursor(1, 14);
     oled_write(keylog, false);
 }
+
 
 void renderMaster(led_t ledUsbState) {
     drawLogo();
@@ -151,6 +153,7 @@ void renderMaster(led_t ledUsbState) {
 // Setup screen
 oled_rotation_t oled_init_kb(oled_rotation_t rotation) {
     startupTimer = timer_read();
+    memcpy_P(&keymap, kbr_full, sizeof(kbr_full));
     return OLED_ROTATION_270;
 }
 
@@ -177,5 +180,100 @@ void setScreenKeys(uint16_t keycode, keyrecord_t *record) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     setScreenKeys(keycode, record);
+
+    if (record->event.pressed) {
+        switch (keycode) {
+            //
+            // Master side
+            case KC_ESC:
+            case KC_1:
+            case KC_Q:
+            case KC_TAB:
+                keymap[0] = 0xC6;
+                break;
+
+            case KC_2:
+            case KC_3:
+            case KC_W:
+            case KC_E:
+                keymap[0] = 0x8A;
+                break;
+
+            case KC_4:
+            case KC_5:
+            case KC_R:
+            case KC_T:
+                keymap[1] = 0xC9;
+                break;
+
+            case KC_A:
+            case KC_S:
+                keymap[0] = 0xAA;
+                break;
+
+            case KC_D:
+            case KC_F:
+                keymap[1] = 0xAB;
+                break;
+
+            case KC_G:
+                keymap[1] = 0xCB;
+                break;
+            case KC_Z:
+            case KC_X:
+                keymap[0] = 0x8C;
+                break;
+            case KC_C:
+            case KC_V:
+                keymap[0] = 0xAC;
+                break;
+            case KC_B:
+                keymap[1] = 0xAD;
+                break;
+
+                //
+                // Slave side
+            case KC_N:
+                keymap[2] = 0x8C;
+                break;
+            case KC_H:
+                keymap[2] = 0xAA;
+                break;
+            case KC_6:
+            case KC_Y:
+                keymap[2] = 0xC6;
+                break;
+            case KC_7:
+            case KC_8:
+            case KC_U:
+            case KC_I:
+                keymap[2] = 0x8A;
+                break;
+            case KC_J:
+            case KC_K:
+                keymap[2] = 0xCA;
+                break;
+            case KC_DOT:
+            case KC_COMM:
+                keymap[3] = 0x8D;
+                break;
+            case KC_SLSH:
+                keymap[3] = 0xAD;
+                break;
+            case KC_L:
+            case KC_SCLN:
+                keymap[3] = 0xCB;
+                break;
+            case KC_O:
+            case KC_P:
+            case KC_9:
+            case KC_0:
+                keymap[3] = 0x8B;
+                break;
+            default:
+                // For other keycodes, you might want to update the keymap accordingly
+                break;
+        }
+    }
     return true; // We didn't handle other keypresses
 }
